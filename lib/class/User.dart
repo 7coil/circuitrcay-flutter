4b306b61d2876a4324bcc51004c31afb3da19b08
Leondro Lio <github@leondrolio.com>
2020-02-07 23:43:19 +0000

    Comment and format the User class

--- lib/class/User.dart
+++ lib/class/User.dart
@@ -34,51 +34,69 @@ class User {
   }
 
   static Future<User> fromCredentials(String username, String password) async {
+    // Circuit Managed Laundry Systems DO NOT take in passwords longer than 64 characters.
+    // Any characters past the 64th char are discarded.
     if (password.length > 64) {
       password = password.substring(0, 64);
     }
 
+    // Encode the username and password, so we can make a POST request
     username = Uri.encodeQueryComponent(username);
     password = Uri.encodeQueryComponent(password);
 
+    // Perform the request
     final response = await http.post(
         'https://laundrymachines.netlify.com/.netlify/functions/fetch/api/user/authenticate?email=$username&password=$password');
 
+    // Parse the body, and create the user
     var json = jsonDecode(response.body);
-
     User user = User.fromJSON(json);
 
+    // If the request returns OK, our body can be saved to the file
+    // Otherwise, return the error
     if (user.ok) {
+      // Get a reference to the 'data.json' file, and write our JSON into the file
       final file = await _localFile;
       await file.writeAsString(response.body);
 
+      // Return the new user
       return user;
     } else {
-      throw(json['Message']);
+      throw (json['Message']);
     }
   }
 
   Future<void> updateBalance() async {
+    // Make a post request with our token to update our balance
     final response = await http.post(
         'https://laundrymachines.netlify.com/.netlify/functions/fetch/api/user/ReconcileCards',
         headers: {'authorization': 'bearer $token'});
 
+    // Parse the body
     var json = jsonDecode(response.body);
 
+    // Update the balance of the user
     if (json['Success'] == true) {
-      this.accountBalance = double.parse(json['Data']['AccountBalance'].toString());
+      this.accountBalance =
+          double.parse(json['Data']['AccountBalance'].toString());
     }
   }
 
-  Future<String> getPaymentURL(int amount, String promotionalCode, bool acceptCreditAndDebit) async {
+  Future<String> getPaymentURL(
+      int amount, String promotionalCode, bool acceptCreditAndDebit) async {
     promotionalCode = Uri.encodeQueryComponent(promotionalCode);
 
+    // Make a post request to retrieve a new PayPal URL
+    // Their query parameters cantDecide BetweenThe casing-theyre-going-to-use
     final response = await http.post(
         'https://laundrymachines.netlify.com/.netlify/functions/fetch/api/user/RequestPurchase?Amount=$amount&PromotionCode=$promotionalCode&billingDirect=$acceptCreditAndDebit',
         headers: {'authorization': 'bearer $token'});
 
+    // Parse the body
     var json = jsonDecode(response.body);
 
+    // If we were successful, return the URL
+    // Otherwise, return the error message.
     if (json['Success'] == true) {
       return json['Data']['PaymentURL'];
     } else {
@@ -87,37 +105,46 @@ class User {
   }
 
   Future<void> updateMachines() async {
+    // Update the list of machines
     machines = await Machine.listMachines(token);
-    print(machines);
   }
 
   Future<void> activateMachine(String id) async {
+    // After activating a machine, update the list of machines
     await Machine.activateMachine(token, id);
     await this.updateMachines();
   }
 
   static Future<String> get _localPath async {
+    // Get a reference to the folder we can store local files in
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
   }
 
   static Future<File> get _localFile async {
+    // Get the reference to the file and folder we can store local files in
     final path = await _localPath;
     File file = File('$path/data.json');
 
+    // If the file doesn't exist at the moment, write nothing
     if (!file.existsSync()) await file.writeAsString("");
 
+    // Return the reference
     return file;
   }
 
   Future<void> logout() async {
+    // Get a reference to the user file
     final file = await _localFile;
+
+    // Overwrite with an empty string
     await file.writeAsString("");
 
     return;
   }
 
   static Future<User> getFromStorage() async {
+    // Get a reference to the user file, and read the contents
     final file = await _localFile;
     String userDataJSON = await file.readAsString();
 
